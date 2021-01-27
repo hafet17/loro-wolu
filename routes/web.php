341eeef675ab589d3e15fc4c08348539d425718e
Hafet17 <do.crazy192@gmail.com>
2021-01-27 23:18:01 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\{Route, Auth};
-use App\Http\Controllers\{HomeController, DashboardController, UserController};
-use App\Http\Controllers\Band\BandController;
+use App\Http\Controllers\{HomeController, UserController};
+use App\Models\{User, Post};
 
 Auth::routes();
 Route::get('/', HomeController::class)->name('home');
 
-Route::get('dashboard', DashboardController::class)->name('dashboard');
+Route::get('{user}/post', function (User $user) {
+    $posts = $user->posts;
+    return view('users.index', [
+        'user' => $user,
+        'posts' => $posts
+    ]);
+});
+
+Route::get('post/{post}', function (Post $post) {
+    return view('users.post', [
+        'post' => $post
+    ]);
+});
+
+
 Route::resource('user', UserController::class);
